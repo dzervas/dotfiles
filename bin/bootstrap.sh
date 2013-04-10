a4e3df8b5d0ecd8695be5441b2d8715250b353a5
Dimitris Zervas <01ttouch@gmail.com>
2013-04-10 17:19:52 +0300

    bootstrap.s almost done, added .xscreensaver

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,12 +2,15 @@
 
 case $1 in
 	install)
-		export backup_dir="$HOME/.backup"
-		export files=`ls -A --ignore=".git{,modules,ignore}" --ignore=".config"`
-		export conf_files=`ls -A .config/`
-		# TODO: create proper symlinks of ~/.config
+		git submodule update --init
+		git submodule foreach git pull origin master
+
+		local backup_dir="$HOME/.backup"
+		local files=`ls -A --ignore=".git{,modules,ignore}" --ignore=".config"`
+		local conf_files=`ls -A .config/`
+
 		for file in $files; do
-			if [ -f "$HOME/$file" ]  || [ -d "$HOME/$file" ] && [ ! -L "$HOME/$file" ]  ; then
+			if [ [ -f "$HOME/$file" ]  || [ -d "$HOME/$file" ] ] && [ ! -L "$HOME/$file" ] ; then
 				echo "$file exists, moving to $backup_dir"
 				if [ ! -d $backup_dir ]; then
 					mkdir -p $backup_dir
@@ -16,19 +19,26 @@ case $1 in
 			fi
 			ln -s "`pwd`/$file" $HOME/
 		done
-		
-		ln -s .config/???* "$HOME/.config/"
-		git submodule update --init
-		git submodule foreach git pull origin master
+
+		for file in $conf_files; do
+			if [ [ -f "$HOME/.config/$file" ]  || [ -d "$HOME/.config/$file" ] ] && [ ! -L "$HOME/.config/$file" ] ; then
+				echo ".config/$file exists, moving to ${backup_dir}/.config"
+				if [ ! -d $backup_dir/.config ]; then
+					mkdir -p $backup_dir/.config
+				fi
+				mv "$HOME/.config/$file" "$backup_dir/.config/"
+			fi
+			ln -s "`pwd`/.config/$file" $HOME/.config/
+		done
 		;;
 	uninstall)
-		echo "You have to do that manually, or contribute to this script. Lazy dog."
+		echo "You have to do that manually or contribute to this script to make it automatic, you lazy dog!"
 		;;
 	system)
 		echo "Updating system..."
 		pacman -Syu
 		echo "Installing X.Org server, WM and some other things..."
-		pacman -S yajl git vim xorg-server{,-utils} xorg-xinit mesa ttf-dejavu xterm slim awesome firefox skype
+		pacman -S yajl git gvim xorg-server{,-utils} xorg-xinit ttf-dejavu xterm slim awesome firefox skype xscreensaver
 		echo "Instaling yaourt..."
 		mkdir -p tmp/{package-query,yaourt}
 		cd tmp/package-query
@@ -40,11 +50,15 @@ case $1 in
 		makepkg --asroot
 		pacman -U *xz
 		cd ../..
-		echo "Installing Technic Launcher via AUR"
-		yaourt -S tekkit
+		echo "Installing Minecraft and minecraft lwjgl fix via AUR"
+		yaourt -S minecraft minecraft-updatelwjgl adb
 		echo "Enabling Slim on boot"
 		systemctl enable slim.service
-		echo "I'm done. Install the appropriate graphics driver and then reboot."
+		systemctl enable xscreensaver.service
+		echo "I'm done. TODO:"
+		echo "\t- Install graphics driver."
+		echo "\t- Configure network."
+		echo "\t- Configure slim."
 		;;
 	*)
 		echo "What do you want me to do? Not even an argument!?"
