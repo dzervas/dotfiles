544bc7f04f39cf38b85062b119595d991e7e1d00
Dimitris Zervas <dzervas@dzervas.gr>
2015-08-28 15:18:01 +0300

    Added php-mode (and some other things)

---
+++ .emacs.d/elpa/s-20150825.50/s-autoloads.el
@@ -0,0 +1,15 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("s.el") (21984 20313 34450 406000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; s-autoloads.el ends here
