4b8d13fc765bfa9c46310c8a07a6334acb8276be
Dimitris Zervas <dzervas@dzervas.gr>
2016-09-04 17:37:49 +0300

    Removed a bunch of crap I no longer use

--- .emacs.d/configs/packages.el
+++
@@ -1,8 +0,0 @@
-(require 'package)
-(push '("gnu" . "http://elpa.gnu.org/packages/"), package-archives)
-(push '("marmalade" . "http://marmalade-repo.org/packages/"), package-archives)
-(push '("melpa" . "http://melpa.milkbox.net/packages/"), package-archives)
-(push '("org" . "http://orgmode.org/elpa/"), package-archives)
-(package-initialize)
-
-(provide 'packages)
--- .gvimrc
+++ .gvimrc
@@ -1,4 +1,4 @@
 set guioptions-=m  "remove menu bar
 set guioptions-=T  "remove toolbar
 set guioptions-=r  "remove right-hand scroll bar
-set guifont=Consolas\ 11
+set guifont=Consolas\ 14
