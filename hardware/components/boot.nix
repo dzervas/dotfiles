a91833eebf7cfb03678eacbcdcd97bb3a2ea4aa2
Dimitris Zervas <dzervas@dzervas.gr>
2024-07-22 20:17:11 +0300

    Fix plymouth

--- hardware/components/boot.nix
+++ hardware/components/boot.nix
@@ -1,16 +1,20 @@
 { ... }: {
   boot = {
+    consoleLogLevel = 0;
+    kernelParams = [ "quiet" "udev.log_level=3" ];
+
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+      verbose = false;
+      systemd.enable = true;
+    };
+
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot = {
         enable = true;
         configurationLimit = 5;
       };
-    };
-
-    initrd = {
-      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-      systemd.enable = true;
     };
 
     plymouth.enable = true;
