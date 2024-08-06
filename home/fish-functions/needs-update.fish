18fc766401b7a3d97298427f2940259b3a652fdd
Dimitris Zervas <dzervas@dzervas.gr>
2024-08-07 01:40:04 +0300

    Add needs upgrade script
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

---
+++ home/fish-functions/needs-update.fish
@@ -0,0 +1,21 @@
+set -f flake_dir (echo $FLAKE_URL | cut -d'?' -f 1 | cut -d'#' -f 1)
+set -f flake_lock "$flake_dir/flake.lock"
+
+set -f current_sha (jq -r '.nodes.nixpkgs.locked.rev' $flake_lock)
+set -f owner (jq -r '.nodes.nixpkgs.locked.owner' $flake_lock)
+set -f repo (jq -r '.nodes.nixpkgs.locked.repo' $flake_lock)
+
+function curl-gh
+	curl -s -H "Accept: application/vnd.github+json" "https://api.github.com/repos/$owner/$repo/$argv"
+end
+
+set -f current_commit_date (curl-gh "commits/$current_sha" | jq -r '.commit.committer.date')
+set -f commits (curl-gh "commits?sha=$branch&per_page=1&since=$current_commit_date" | jq -r '. | length')
+
+if test $commits -gt 0
+	echo "Needs update"
+	return 1
+else
+	echo "Up to date"
+	return 0
+end
