85090995a7801ee27ae1d1433f6bc98250c09558
Dimitris Zervas <dzervas@dzervas.gr>
2024-08-09 02:57:36 +0300

    Update

--- hardware/components/virtualbox.nix
+++ hardware/components/virtualbox.nix
@@ -1,7 +1,7 @@
 { ... }: {
   virtualisation.virtualbox.host = {
     enable = true;
-    enableKvm = true;
+    # enableKvm = true;
     enableExtensionPack = true;
 
     # Can't co-exist with enableKvm
