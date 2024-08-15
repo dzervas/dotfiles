34c3289ca512cc77db85d2295f545d707bc525e8
Dimitris Zervas <dzervas@dzervas.gr>
2024-08-15 23:31:34 +0300

    Add direnvrc
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- home/direnv.nix
+++ home/direnv.nix
@@ -2,6 +2,7 @@ _: {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    stdlib = builtins.readFile ./direnvrc;
     config = {
       global = {
         strict_env = true; # set -euo pipefail
