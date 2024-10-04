59908b09ba18a53e402a21190bd51b797d2fe4dd
Dimitris Zervas <dzervas@dzervas.gr>
2024-10-04 22:46:19 +0300

    Add the bwrapper module
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   imports = [
     ./1password.nix
     ./alacritty.nix
@@ -18,11 +18,37 @@
     ./thumbnailers.nix
     ./wine
     ./desktop/sway.nix
+    ../modules/bwrapper.nix
   ];
 
   programs.mpv.enable = true;
-
   programs.home-manager.enable = true;
+
+  bwrapper.prismlauncher = {
+    # customCommand = "${pkgs.prismlauncher}/bin/prismlauncher";
+    dev = "/dev";
+    proc = "/proc";
+    dev-bind = [ { src = "/dev/dri"; dest = "/dev/dri"; } ];
+
+    ro-bind = [
+      { src = "/etc"; dest = "/etc"; }
+      { src = "/nix"; dest = "/nix"; }
+      { src = "/tmp/.X11-unix"; dest = "/tmp/.X11-unix"; }
+      { src = "/tmp/.ICE-unix"; dest = "/tmp/.ICE-unix"; }
+      { src = "/run/opengl-driver"; dest = "/run/opengl-driver"; }
+      # --ro-bind-try "$XDG_RUNTIME_DIR/pulse" /tmp/pulse \
+      # --ro-bind-try "$XDG_RUNTIME_DIR/pipewire-0" /tmp/pipewire-0 \
+    ];
+    bind = [
+      { src = "${config.home.homeDirectory}/.local/share/PrismLauncher"; dest = "${config.home.homeDirectory}/.local/share/PrismLauncher"; }
+    ];
+    tmpfs = [ "/tmp" ];
+
+    unshare-all = true;
+    share-net = true;
+    die-with-parent = true;
+  };
+
   home = {
     username = "dzervas";
     homeDirectory = "/home/dzervas";
@@ -34,7 +60,6 @@
       vorta
 
       bubblewrap
-      prismlauncher
     ];
     file.".config/katerc".source = ./katerc;
   };
---
+++ modules/bwrapper.nix
@@ -0,0 +1,127 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkOption types;
+
+  buildBwrapArgs = pkgConfig: with pkgConfig;
+    let
+      bindArgs = builtins.concatMap (mount:
+        [ "--bind${if mount.try or false then "-try" else ""}" mount.src mount.dest ]
+      ) bind;
+      devBindArgs = builtins.concatMap (mount:
+        [ "--dev-bind${if mount.try or false then "-try" else ""}" mount.src mount.dest ]
+      ) dev-bind;
+      roBindArgs = builtins.concatMap (mount:
+        [ "--ro-bind${if mount.try or false then "-try" else ""}" mount.src mount.dest ]
+      ) ro-bind;
+      tmpfsArgs = builtins.concatMap (path: [ "--tmpfs" path ]) tmpfs;
+      procArg = if proc != "" then [ "--proc" proc ] else [];
+      devArg = if dev != "" then [ "--dev" dev ] else [];
+      newSessionArg = if new-session then [ "--new-session" ] else [];
+      dieWithParentArg = if die-with-parent then [ "--die-with-parent" ] else [];
+      unshareAllArg = if unshare-all then [ "--unshare-all" ] else [];
+      shareNetArg = if share-net then [ "--share-net" ] else [];
+    in
+      bindArgs ++ devBindArgs ++ roBindArgs ++ tmpfsArgs ++ procArg ++ devArg ++ newSessionArg ++ unshareAllArg ++ shareNetArg ++ dieWithParentArg;
+
+  wrapPackage = { name, package, pkgConfig }:
+    pkgs.stdenv.mkDerivation {
+      pname = "${name}-bwrapped";
+      version = package.version or "unknown";
+      buildInputs = [ pkgs.bubblewrap ];
+      dontUnpack = true;
+      installPhase = ''
+        mkdir -p $out/bin
+        ${if pkgConfig.customCommand != null then ''
+        cat > $out/bin/${name} <<EOF
+        #!${pkgs.runtimeShell}
+        exec ${pkgs.bubblewrap}/bin/bwrap ${lib.strings.concatStringsSep " " (buildBwrapArgs pkgConfig)} -- ${pkgConfig.customCommand} "\$@"
+        EOF
+        chmod +x $out/bin/${name}
+        '' else ''
+        for bin in ${package}/bin/*; do
+          if [ -x "$bin" ]; then
+            progName=$(basename $bin)
+            cat > $out/bin/$progName <<EOF
+        #!${pkgs.runtimeShell}
+        exec ${pkgs.bubblewrap}/bin/bwrap ${lib.strings.concatStringsSep " " (buildBwrapArgs pkgConfig)} -- "$bin" "\$@"
+        EOF
+            chmod +x $out/bin/$progName
+          fi
+        done
+        ''}
+      '';
+    };
+
+in {
+  options.bwrapper = mkOption {
+    type = types.attrsOf (types.submodule {
+      options = {
+        bind = mkOption {
+          type = types.listOf (types.attrsOf types.anything);
+          default = [];
+          description = "List of bind mounts to pass to bwrap";
+        };
+        die-with-parent = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Kills with SIGKILL child process (COMMAND) when bwrap or bwrap's parent dies.";
+        };
+        dev-bind = mkOption {
+          type = types.listOf (types.attrsOf types.anything);
+          default = [];
+          description = "List of dev-bind mounts to pass to bwrap";
+        };
+        ro-bind = mkOption {
+          type = types.listOf (types.attrsOf types.anything);
+          default = [];
+          description = "List of ro-bind mounts to pass to bwrap";
+        };
+        proc = mkOption {
+          type = types.str;
+          default = "";
+          description = "Proc option for bwrap";
+        };
+        dev = mkOption {
+          type = types.str;
+          default = "";
+          description = "Dev option for bwrap";
+        };
+        tmpfs = mkOption {
+          type = types.listOf types.str;
+          default = [];
+          description = "List of tmpfs options for bwrap";
+        };
+        new-session = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Whether to use new-session option";
+        };
+        unshare-all = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Whether to unshare all namespaces";
+        };
+        share-net = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Whether to share network namespace";
+        };
+        customCommand = mkOption {
+          type = types.nullOr types.str;
+          default = null;
+          description = "Custom command to run if binary is not found";
+        };
+      };
+    });
+    description = "Per-package bwrap configurations";
+  };
+
+  # config.bwrapper.wrappers = lib.attrValues (lib.mapAttrs (pkgName: pkgConfig:
+  config.home.packages = lib.attrValues (lib.mapAttrs (pkgName: pkgConfig:
+    let
+      package = pkgs.${pkgName} or (throw "Package ${pkgName} not found in pkgs");
+    in
+      wrapPackage { inherit package pkgConfig; name = pkgName; }
+  ) config.bwrapper);
+}
