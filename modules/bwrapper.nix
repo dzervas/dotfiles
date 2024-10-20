63fef359189f27687fecbceacc04118d3e96f9de
Dimitris Zervas <dzervas@dzervas.gr>
2024-10-20 20:05:45 +0300

    Fix bwrapper
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- modules/bwrapper.nix
+++ modules/bwrapper.nix
@@ -14,17 +14,17 @@ let
 
       # Bind mount options
       bind = mkOption {
-        type = types.listOf (types.attrsOf bindTryOption);
+        type = types.listOf bindTryOption;
         default = [];
         description = "List of bind mounts to pass to bwrap";
       };
       dev-bind = mkOption {
-        type = types.listOf (types.attrsOf bindTryOption);
+        type = types.listOf bindTryOption;
         default = [];
         description = "List of dev-bind mounts to pass to bwrap";
       };
       ro-bind = mkOption {
-        type = types.listOf (types.attrsOf bindTryOption);
+        type = types.listOf bindTryOption;
         default = [];
         description = "List of ro-bind mounts to pass to bwrap";
       };
@@ -101,63 +101,76 @@ let
     };
   };
 
-  bindTryArg = prefix: opt: builtins.concatMap (opt: [
-    "${prefix}${if opt.try then "-try" else ""}" opt.src (if opt.dest != null then opt.dest else opt.src) "\n"
-  ]);
+  bindTryArg = prefix: opts:
+    (builtins.concatMap (opt:
+      let
+        src = if opt.src == null then throw "src is required" else opt.src;
+        dest = if opt.dest == null then src else opt.dest;
+        try = opt.try or false;
+      in [ "${prefix}${if try then "-try" else ""} ${src} ${dest}" ]
+    ) opts);
 
   # Construct the bwrap command line arguments based on the options above as a list
   # Takes a single package config as argument
-  buildBwrapArgs = pkgConfig: with pkgConfig;
-    (if new-session then [ "--new-session" ] else []) ++ "\n" ++
-    (if die-with-parent then [ "--die-with-parent" ] else []) ++ "\n" ++
-    (if unshare-all then [ "--unshare-all" ] else []) ++ "\n" ++
-    (if share-net then [ "--share-net" ] else []) ++ "\n" ++
-    (if dev != "" then [ "--dev" dev ] else []) ++ "\n" ++
-    (if proc != "" then [ "--proc" proc ] else []) ++ "\n" ++
-    builtins.concatMap (path: [ "--tmpfs" path ]) tmpfs ++ "\n" ++
-    lib.attrsets.mapAttrsToList (name: value: "--setenv ${name} ${value}") setenv ++ "\n" ++
-    builtins.concatMap (value: [ "--unsetenv" value ]) unsetenv ++ "\n" ++
-    bindTryArg "--dev-bind" dev-bind ++
-    bindTryArg "--ro-bind" ro-bind ++
-    bindTryArg "--bind" bind;
+  buildBwrapArgs = pkgConfig:
+    (if pkgConfig.new-session then [ "--new-session" ] else []) ++
+    (if pkgConfig.die-with-parent then [ "--die-with-parent" ] else []) ++
+    (if pkgConfig.unshare-all then [ "--unshare-all" ] else []) ++
+    (if pkgConfig.share-net then [ "--share-net" ] else []) ++
+    (if pkgConfig.dev != "" then [ "--dev" pkgConfig.dev ] else []) ++
+    (if pkgConfig.proc != "" then [ "--proc" pkgConfig.proc ] else []) ++
+    builtins.concatMap (path: [ "--tmpfs" path ]) pkgConfig.tmpfs ++
+    lib.attrsets.mapAttrsToList (name: value: "--setenv ${name} ${value}") pkgConfig.setenv ++
+    builtins.concatMap (value: [ "--unsetenv" value ]) pkgConfig.unsetenv ++
+    bindTryArg "--dev-bind" pkgConfig.dev-bind ++
+    bindTryArg "--ro-bind" pkgConfig.ro-bind ++
+    bindTryArg "--bind" pkgConfig.bind;
 
   # Create the actual overlay
   wrapPackage = { name, package, pkgConfig }:
-    (super: self: self.stdenv.mkDerivation {
-      pname = "${package.pname or name}-bwrap";
-      version = package.version or "unknown";
-      buildInputs = [ super.bubblewrap ];
-      dontUnpack = true;
-      installPhase = let
-        bwrapArgs = lib.strings.concatStringsSep " " (buildBwrapArgs pkgConfig);
-      in ''
-        mkdir -p $out/bin
-        ${if builtins.hasAttr "customCommand" pkgConfig then ''
-        cat > $out/bin/${name} <<EOF
-        #!${super.runtimeShell}
-        exec ${super.bubblewrap}/bin/bwrap ${bwrapArgs} -- ${pkgConfig.customCommand} "\$@"
-        EOF
-        chmod +x $out/bin/${name}
-        '' else ''
-        for bin in ${package}/bin/*; do
-          if [ -x "$bin" ]; then
-            progName=$(basename $bin)
-            cat > $out/bin/$progName <<EOF
-        #!${super.runtimeShell}
-        exec ${super.bubblewrap}/bin/bwrap ${bwrapArgs} -- "$bin" "\$@"
-        EOF
-            chmod +x $out/bin/$progName
-          fi
-        done
-        ''}
-      '';
-  });
+    (super: self: let
+      pname = "${package.pname or name}";
+    in {
+      "${pname}" = self.stdenv.mkDerivation rec {
+        inherit pname;
+
+        src = package;
+        version = package.version or "unknown";
+        buildInputs = [ super.bubblewrap ];
+        dontUnpack = true;
+        installPhase = let
+          bwrapArgs = lib.strings.concatStringsSep " " (buildBwrapArgs pkgConfig);
+        in ''
+          mkdir -p $out/bin
+          ${if pkgConfig.customCommand != null then ''
+          cat > $out/bin/${name} <<EOF
+          #!${super.runtimeShell}
+          exec ${super.bubblewrap}/bin/bwrap ${bwrapArgs} -- ${pkgConfig.customCommand} "\$@"
+          EOF
+          chmod +x $out/bin/${name}
+          '' else ''
+          for bin in ${src}/bin/*; do
+            if [ -x "$bin" ]; then
+              progName=$(basename $bin)
+              cat > $out/bin/$progName <<EOF
+          #!${super.runtimeShell}
+          exec ${super.bubblewrap}/bin/bwrap ${bwrapArgs} -- "$bin" "\$@"
+          EOF
+              chmod +x $out/bin/$progName
+            fi
+          done
+          ''}
+        '';
+      };
+    }
+  );
 
   # Return nixpkgs with the generated overlay
-  wrapPackageOverlay = { name, package, pkgConfig }: import inputs.nixpkgs {
+  wrapPackageOverlay = { name, package, pkgConfig }: (import inputs.nixpkgs {
     inherit (pkgs) system;
+
     overlays = [ (wrapPackage { inherit name package pkgConfig; }) ];
-  };
+  });
 in {
   # Define the bwrapper configuration
   options.bwrapper = mkOption {
@@ -174,7 +187,7 @@ in {
     (name: pkgConfig: let
         package = pkgs.${name} or (throw "Package ${name} not found in pkgs");
       in
-        (wrapPackageOverlay { inherit name package pkgConfig; }).${name}
+        (wrapPackageOverlay { inherit name package pkgConfig; })."${name}"
     )
 
     config.bwrapper
