b98fba1ba017f0755907db78d5e8e84753a22608
Dimitris Zervas <dzervas@dzervas.gr>
2024-11-15 02:29:59 +0200

    Update vscode

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,8 +7,8 @@ _: {
         plat = "linux-x64";
 
         # gha-updater: VERSION="$(curl https://update.code.visualstudio.com/api/releases/stable | jq -r '. | first')" && echo -n "$VERSION $(nix-prefetch-url https://update.code.visualstudio.com/$VERSION/linux-x64/stable)"
-        version = "1.95.2";
-        sha256 = "1d99kbvbd17h02m85gd8dircb39jsbifdk61w1hsvhri6dbfah8f";
+        version = "1.95.3";
+        sha256 = "0ijv2y2brc05m45slsy24dp8r0733d89f082l3mbs64vkz76s748";
 
         src = prev.fetchurl {
           inherit sha256;
