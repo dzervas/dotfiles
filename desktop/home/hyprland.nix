fa6ac5d01d5aed4de92c6b2f6947f62f19b3987a
Dimitris Zervas <dzervas@dzervas.gr>
2025-04-06 22:36:55 +0300

    Minor touchups

--- desktop/home/hyprland.nix
+++ desktop/home/hyprland.nix
@@ -22,6 +22,7 @@ in {
       settings = {
         general = {
           lock_cmd = "hyprlock";
+          before_sleep_cmd = "hyprlock";
           after_sleep_cmd = "hyprctl dispatch dpms on";
         };
         listener = [
--- hardware/laptop.nix
+++ hardware/laptop.nix
@@ -67,6 +67,7 @@ in {
 
   environment.systemPackages = with pkgs; [
     powertop
+    framework-tool
   ];
 
   # powerManagement = {
