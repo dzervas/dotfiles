e7affd7ca093ebef23c492668a32131022069087
Dimitris Zervas <dzervas@dzervas.gr>
2025-05-15 01:22:00 +0300

    Rollback buggy vscode and update private repo

--- desktop/home/hyprland.nix
+++ desktop/home/hyprland.nix
@@ -145,6 +145,7 @@ in {
         { class = "OrcaSlicer"; rules = ["suppressevent"]; }
         { class = "org.pulseaudio.pavucontrol"; rules = ["float" "center"]; }
         { title = "Ropuka's Idle Island"; rules = ["float" "persistentsize" "fullscreenstate 0 0"]; }
+        { title = "atuin-desktop"; rules = ["float"]; }
       ];
 
       # Layouts
