d68e6132e5f6ff3c2cb707230eacf5a6750b6534
Dimitris Zervas <dzervas@dzervas.gr>
2025-06-16 00:38:17 +0300

    Fix pico8

--- overlays/pico8.nix
+++ overlays/pico8.nix
@@ -2,6 +2,8 @@
 { pkgs, ...}: with pkgs; stdenv.mkDerivation rec {
   name = "pico-8";
   src = ../home/.private/pico-8_0.2.6b_amd64.zip;
+
+  nativeBuildInputs = [copyDesktopItems];
   buildInputs = [
     unzip
     patchelf
@@ -33,13 +35,6 @@
   in "${lib.makeLibraryPath sdl_load_path}";
 
   unpackPhase = "unzip $src";
-  installPhase = ''
-    mkdir $out
-    cp pico-8/* $out
-    mkdir $out/bin
-    echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $out/pico8" > $out/bin/pico8
-    chmod 555 $out/bin/pico8
-  '';
 
   preFixup = ''
     patchelf \
@@ -50,5 +45,25 @@
       --replace-needed \
        libSDL2-2.0.so.0 $SDL2/lib/libSDL2-2.0.so.0 \
        $out/pico8_dyn
+  '';
+
+  desktopItems = [(makeDesktopItem {
+    name = "Pico 8";
+    exec = "pico8 -splore";
+    icon = "pico8";
+    desktopName = "Pico 8";
+    categories = [ "Development" "Game" ];
+  })];
+
+  installPhase = ''
+    mkdir $out
+    cp pico-8/* $out
+
+    mkdir $out/bin
+    echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $out/pico8 \$@" > $out/bin/pico8
+    chmod 555 $out/bin/pico8
+
+    mkdir -p $out/share/icons/hicolor/scalable/apps
+    cp pico-8/lexaloffle-pico8.png $out/share/icons/hicolor/scalable/apps/pico8.png
   '';
 }
