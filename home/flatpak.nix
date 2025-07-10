510a4ab3902cb110f11fe73c0534385825635e18
Dimitris Zervas <dzervas@dzervas.gr>
2025-07-10 23:55:51 +0300

    Signal wayland

--- home/flatpak.nix
+++ home/flatpak.nix
@@ -33,7 +33,10 @@ _: {
         filesystems = [ "home/CryptVMs" ];
       };
       # "org.chromium.Chromium".Context.sockets = ["wayland"];
-      "org.signal.Signal".Context.sockets = ["x11"];
+      "org.signal.Signal".Environment = {
+        SIGNAL_PASSWORD_STORE = "gnome-libsecret";
+        SIGNAL_USE_WAYLAND = "1";
+      };
       "org.onlyoffice.desktopeditors".Context.sockets = ["x11"];
       "md.obsidian.Obsidian".Context.filesystems = [ "xdg-documents/Obsidian" ];
       "com.slack.Slack".Context.sockets = ["wayland" "pulseaudio"];
