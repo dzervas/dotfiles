7b9291152bc3399bc866012acb3e0537190a0bf6
Dimitris Zervas <dzervas@dzervas.gr>
2025-08-19 11:44:22 +0100

    Get openvpn-aws to connect

--- overlays/openvpn-aws/default.nix
+++ overlays/openvpn-aws/default.nix
@@ -1,10 +1,12 @@
 _final: prev:
-prev.openvpn.overrideAttrs (prevAttrs: {
+prev.openvpn.overrideAttrs (prevAttrs: rec {
   pname = "openvpn-aws";
   patches = (prevAttrs.patches or []) ++ [
     # From https://github.com/samm-git/aws-vpn-client/blob/master/openvpn-v2.6.12-aws.patch
     ./openvpn-aws-v2.6.12.patch
   ];
+
+  script = ./connect.sh;
 
   # install an extra binary named openvpn-aws (and its manpage symlink)
   postInstall = (prevAttrs.postInstall or "") + ''
@@ -14,11 +16,13 @@ prev.openvpn.overrideAttrs (prevAttrs: {
       mv "$out/sbin/openvpn" "$out/bin/openvpn"
     fi
 
-    cp "$out/bin/openvpn" "$out/bin/openvpn-aws"
+    mv "$out/bin/openvpn" "$out/bin/openvpn-aws"
+    cp "${script}" $out/bin/openvpn-aws-connect
+    chmod +x $out/bin/openvpn-aws-connect
 
     # Man page (if present)
     if [ -e "$out/share/man/man8/openvpn.8.gz" ]; then
-      ln -s "openvpn.8.gz" "$out/share/man/man8/openvpn-aws.8.gz" || true
+      mv "openvpn.8.gz" "$out/share/man/man8/openvpn-aws.8.gz" || true
     fi
   '';
 
