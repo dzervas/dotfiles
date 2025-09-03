a3eb803dc5a683d2b6bec837d3f45c45601b1175
Dimitris Zervas <dzervas@dzervas.gr>
2025-09-04 02:59:18 +0300

    Fix the fucking lockscreen minutes

--- desktop/home/components/hyprlock.nix
+++ desktop/home/components/hyprlock.nix
@@ -33,7 +33,7 @@
 
       label = [
       {
-        text = "cmd[update:100] date +'%H:%m'";
+        text = "cmd[update:100] date +'%H:%M'";
 
         font_size = 80;
         font_family = "Iosevka Term Extrabold";
