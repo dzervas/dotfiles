09779505ae93536613eb61cefbe8fd08adcd9343
Dimitris Zervas <dzervas@dzervas.gr>
2025-09-10 09:52:43 +0300

    Add treesitter custom injection - doesn't work yet

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -432,6 +432,8 @@
       # None-ls packages
       gomodifytags
       impl
+      yamllint
+      codespell
 
       ncurses # infocmp bin
       vimPlugins.vim-floaterm # provides the 'floaterm' helper script on PATH
@@ -506,6 +508,26 @@
         # luaLib = true;
         # plugins = true;
       };
+    };
+
+
+    # Treesitter language injections without Lua: drop query files into runtimepath
+    extraFiles = {
+      # - YAML: inject highlighting into block scalars based on key hints
+      "after/queries/yaml/injections.scm".text = ''
+        ; extends
+        ; Inject language into block scalars based on key names
+        ; Values like `values: |` or `manifest: |` will be treated as YAML.
+        ; From https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/yaml/injections.scm
+        (block_mapping_pair
+           key: (flow_node) @_yaml
+           (#any-of? @_yaml "yaml" "manifest" "values")
+           value: (block_node
+                    (block_scalar) @injection.content
+                    (#set! injection.language "yaml")
+                    (#set! injection.include-children)
+                    (#offset! @injection.content 0 1 0 0)))
+      '';
     };
   };
 
