0f24657eddc56d1999add543600da09330e354e1
Dimitris Zervas <dzervas@dzervas.gr>
2025-09-30 16:03:36 +0300

    make acp smarter

--- home/git.nix
+++ home/git.nix
@@ -19,16 +19,16 @@
       aliases = {
         aa = "!git add -A && git status";
         ac = "!git aa && git commit";
-        acp = "!git ac && git push";
+        acp = ''!f(){ if test $# -gt 1; then git aa && git commit -m "$*" && git push; else git ac && git push; fi }'';
         bl = "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr($2, 1, length($2)-1), $1)}'";
         c = "clone";
         co = "checkout";
         d = "diff";
-# get-ignore = "!"f(){ curl -L --silent --fail "https://github.com/github/gitignore/raw/main/$1.gitignore" >> .gitignore && echo "Appended to .gitignore" || echo -e "No gitignore found for $1 - check out https://github.com/github/gitignore"; }; f"";
+        # get-ignore = "!"f(){ curl -L --silent --fail "https://github.com/github/gitignore/raw/main/$1.gitignore" >> .gitignore && echo "Appended to .gitignore" || echo -e "No gitignore found for $1 - check out https://github.com/github/gitignore"; }; f"";
         get-ignore = "!gi(){ curl -fsL \"https://www.toptal.com/developers/gitignore/api/$1\" >> .gitignore && echo \"Appended to .gitignore\" || echo \"No gitignore found - check out gitignore.io\"; }; gi";
         ll = "log --graph --decorate --abbrev-commit --pretty='%C(auto)%h %d %s %Cgreen(%cr)%Creset [%C(bold blue)%an%Creset %G?]'";
         lla = "log --graph --decorate --abbrev-commit --pretty='%C(auto)%h %d %s %Cgreen(%cr)%Creset [%C(bold blue)%an%Creset  %G?]' --all";
-# Parse positional params
+        # Parse positional params
         hub = "!f() { grep -q '/' <<< $1 && git clone git@github.com:$1 || git clone git@github.com:dzervas/$1; }; f";
         oops = "!echo 'Going to amend and force push. You sure?' && read && git add -A && git commit --amend --no-edit && git push --force-with-lease";
         s = "status";
@@ -44,7 +44,7 @@
         web.browser = "firefox";
         pull.rebase = true;
 
-# Output stuff
+        # Output stuff
         branch.sort = "-committerdate";
         log.date = "local";
         tag.sort = "version:refname";
