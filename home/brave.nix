b9f70ac4d2483d682f35cffe266baee0b7124a01
Dimitris Zervas <dzervas@dzervas.gr>
2025-11-30 03:38:31 +0200

    Remove brave config in favor of brave sync

--- home/brave.nix
+++ home/brave.nix
@@ -9,33 +9,5 @@
   programs.chromium = {
     enable = true;
     package = pkgs.brave;
-
-    extensions = [
-      { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
-
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
-      { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
-      { id = "enamippconapkdmgfgjchkhakpfinmaj"; } # DeArrow
-      { id = "cdglnehniifkbagbbombnjghhcihifij"; } # Kagi
-    ];
-
-    # https://github.com/ungoogled-software/ungoogled-chromium/blob/master/docs/flags.md
-    commandLineArgs = [
-      # Behavior
-      "--disable-default-browser-promo"
-      "--enable-logging=stderr"
-      "--restore-last-session"
-      "--extension-mime-request-handling=always-prompt-for-install"
-      "--force-dark-mode"
-      "--keep-old-history"
-
-      # Wayland shit
-      # TODO: Move to wayland-fixes
-      "--enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,ReducedSystemInfo"
-      "--ozone-platform=wayland"
-
-      # Most recent tab switcher
-      "--enable-features=CtrlTabMRU"
-    ];
   };
 }
