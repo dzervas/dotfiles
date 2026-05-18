61681b0d4bad4175bb6c564fbcbb8fb63eac983b
Dimitris Zervas <dzervas@dzervas.gr>
2026-05-18 18:40:28 +0300

    Remove leftover blueman

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -22,9 +22,6 @@
 
   boot.kernelParams = [ "btusb.enable_autosuspend=n" ];
 
-  # Needed for home-manager's blueman
-  services.blueman.enable = true;
-
   # Better Xbox gamepad driver
   # hardware.xpadneo.enable = true;
 
